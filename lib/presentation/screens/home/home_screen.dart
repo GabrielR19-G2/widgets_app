fb1732cb48f6441b09ad27646481c03d6cce7130
GabrielR19-G2 <79234237+GabrielR19-G2@users.noreply.github.com>
2024-12-22 19:10:05 -0700

    rutas con nombre implementado

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
+import 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
 
 class HomeScreen extends StatelessWidget {
+  static const name = 'home_screen';
+
   const HomeScreen({super.key});
 
   @override
@@ -55,7 +58,7 @@ class _CustomListTile extends StatelessWidget {
       subtitle: Text(menuItem.subTitle),
       onTap: () {
         print(menuItem.title);
-        // TODO: Navegar a otras pantallas
+        // TODO: Navegar a otras pantallas con Flutter
         // Navigator.of(context).push(
         //   MaterialPageRoute(
         //     builder: (context) => const ButtonsScreen(),
@@ -64,7 +67,9 @@ class _CustomListTile extends StatelessWidget {
         // Navigator.pushNamed(context, menuItem.link);
 
         /// Usando goRoute
-        context.push(menuItem.link);
+        // context.push(menuItem.link);
+        /// Con name
+        context.pushNamed(CardsScreen.name);
       },
     );
   }
