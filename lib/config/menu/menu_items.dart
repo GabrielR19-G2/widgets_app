ae40f92632b1656ffcbdbead5b8208fce0e590b0
GabrielR19-G2 <79234237+GabrielR19-G2@users.noreply.github.com>
2024-12-25 22:58:01 -0700

    progress Indicators finished

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -28,4 +28,9 @@ const appMenuItems = <MenuItem>[
       subTitle: 'Un contenedor estilizado',
       link: '/cards',
       icon: Icons.credit_card),
+  MenuItem(
+      title: 'ProgressIndicators',
+      subTitle: 'Generales y controlados',
+      link: '/progress',
+      icon: Icons.refresh_rounded),
 ];
--- lib/presentation/screens/progress/progress_screen.dart
+++ lib/presentation/screens/progress/progress_screen.dart
@@ -6,6 +6,73 @@ class ProgressScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Progress Indicators'),
+      ),
+      body: const _ProgressView(),
+    );
+  }
+}
+
+class _ProgressView extends StatelessWidget {
+  const _ProgressView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+        child: Column(
+      children: [
+        SizedBox(height: 30),
+        Text('Circular progress indicator'),
+        SizedBox(height: 10),
+        CircularProgressIndicator(
+            strokeWidth: 2, backgroundColor: Colors.black45),
+        SizedBox(height: 20),
+        Text('Circular y linear controlado'),
+
+        /// Basado en porcentaje
+        _ControlledProgressIndicator()
+      ],
+    ));
+  }
+}
+
+class _ControlledProgressIndicator extends StatelessWidget {
+  const _ControlledProgressIndicator();
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      // Se va a construir en tiempo de ejecución, stream -> flujo de información
+      stream: Stream.periodic(const Duration(milliseconds: 300), (value) {
+        return (value * 2) / 100;
+      }).takeWhile(
+          (value) => value < 100), //Recibe valores hasta que sea menor que 100
+      builder: (context, snapshot) {
+        //Snapshot -> nos da el valor que se está emitiendo
+        final progressValue = snapshot.data ?? 0;
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircularProgressIndicator(
+                value: progressValue,
+                strokeWidth: 2,
+                backgroundColor: Colors.black12,
+              ),
+              const SizedBox(width: 20),
+              // Toma todo el espacio que el padre le está dando
+              Expanded(
+                child: LinearProgressIndicator(value: progressValue),
+              )
+            ],
+          ),
+        );
+      },
+    );
   }
 }
